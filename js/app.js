7b0872fdd07f79c5dc2724c9dbfc027fdc23aaa3
invervgit <shivanshu.sharma@inverv.com>
2026-09-23 14:11:07 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -344,7 +344,7 @@
 
     // ========== Fetch via Apps Script Proxy ==========
     async function fetchWithProxy(url) {
-        if (!state.appsScriptUrl || state.appsScriptUrl === 'YOUR_WEB_APP_URL_HERE') {
+        if (!state.appsScriptUrl || state.appsScriptUrl === 'https://script.google.com/a/macros/inverv.com/s/AKfycbwaXjlZWB5REHVn3M7BtuIMam5oPvWj0Iu3oZnTAy-UlWXTbjnUH8LTvpUicYsppHvK8w/exec') {
             throw new Error('Please add your Google Apps Script URL in the js/app.js file.');
         }
 
